acdb11f181bef96febc48a9504685964b4bd22ff
Dmitry Andreev <AndreevDm@yandex-team.ru>
2015-04-09 19:25:05 +0300

    New graphite properties Faster MetricInputStream

--- src/java/ru/yandex/market/graphouse/cacher/MetricInputStream.java
+++ src/java/ru/yandex/market/graphouse/cacher/MetricInputStream.java
@@ -1,9 +1,11 @@
 package ru.yandex.market.graphouse.cacher;
 
+import org.apache.http.util.ByteArrayBuffer;
 import ru.yandex.market.graphouse.Metric;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,9 +21,8 @@ public class MetricInputStream extends InputStream {
     private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
     private List<Metric> metrics;
 
-    private StringBuilder row = new StringBuilder(100);
+    ByteBuffer buffer = ByteBuffer.allocate(2000);
     private int rowNum = 0;
-    private int rowPosition = 0;
 
     public MetricInputStream(List<Metric> metrics) {
         this.metrics = metrics;
@@ -29,13 +30,30 @@ public class MetricInputStream extends InputStream {
 
     @Override
     public int read() throws IOException {
-        if (rowPosition >= row.length()) {
-            readRow();
-        }
-        if (rowPosition >= row.length()) {
+        if (!fillBuffer()) {
             return -1;
         }
-        return row.codePointAt(rowPosition++);
+        return buffer.get();
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        if (!fillBuffer()) {
+            return -1;
+        }
+        int remaining = buffer.remaining();
+        buffer.put(b, off, len);
+        return remaining - buffer.remaining();
+    }
+
+    /**
+     * @return false если данные закончились
+     */
+    private boolean fillBuffer() {
+        if (buffer.remaining() == 0) {
+            readRow();
+        }
+        return buffer.remaining() > 0;
     }
 
     private void readRow() {
@@ -43,17 +61,27 @@ public class MetricInputStream extends InputStream {
             return;
         }
 
-        rowPosition = 0;
-        row.setLength(0);
-
         Metric metric = metrics.get(rowNum);
 
-        row.append(metric.getName()).append('\t');
-        row.append(metric.getValue()).append('\t');
-        row.append(metric.getTime()).append('\t');
-        row.append(dateFormat.format(metric.getUpdated())).append('\t');
-        row.append(getTimestampSeconds(metric.getUpdated())).append('\n');
+        appendBytes(metric.getName().getBytes());
+        buffer.putChar('\t');
+        appendBytes(Double.toString(metric.getValue()).getBytes());
+        buffer.putChar('\t');
+        append(metric.getTime());
+        buffer.putChar('\t');
+        appendBytes(dateFormat.format(metric.getUpdated()).getBytes());
+        buffer.putChar('\t');
+        append(getTimestampSeconds(metric.getUpdated()));
+        buffer.putChar('\n');
         rowNum++;
+    }
+
+    private void append(int data) {
+        appendBytes(Integer.toString(data).getBytes());
+    }
+
+    private void appendBytes(byte[] bytes) {
+        buffer.put(bytes, 0, bytes.length);
     }
 
     private static int getTimestampSeconds(Date date) {
