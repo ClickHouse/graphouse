db8345e025216a84b9957db5f76ccc9952a3944d
Vlad Vinogradov <vladvin@yandex-team.ru>
2016-11-11 11:23:52 +0300

    MARKETHEALTH-589 Test fix

---
+++ src/main/java/ru/yandex/market/graphouse/GraphouseMain.java
@@ -0,0 +1,30 @@
+package ru.yandex.market.graphouse;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Vlad Vinogradov <a href="mailto:vladvin@yandex-team.ru"></a>
+ * @date 10.11.16
+ */
+public class GraphouseMain {
+    private static final Logger log = LogManager.getLogger();
+
+    private GraphouseMain() {
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
+        applicationContext.registerShutdownHook();
+        applicationContext.register(GraphouseMainConfiguration.class);
+        applicationContext.refresh();
+
+        log.info("Graphouse up and running");
+
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("Uncaught Exception in thread " + t.toString(), e));
+    }
+}
---
+++ src/main/java/ru/yandex/market/graphouse/GraphouseMainConfiguration.java
@@ -0,0 +1,13 @@
+package ru.yandex.market.graphouse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author Vlad Vinogradov <a href="mailto:vladvin@yandex-team.ru"></a>
+ * @date 10.11.16
+ */
+@Configuration
+@ImportResource("classpath:property-configurer.xml")
+public class GraphouseMainConfiguration {
+}
