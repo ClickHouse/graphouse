dd21880ed0696870eddf356e21bc5a986e00d2eb
Dmitry Andreev <AndreevDm@yandex-team.ru>
2018-10-26 20:24:57 +0300

    MARKETINFRA-1877 docker run refactor

--- docker/graphouse/run.py
+++ docker/graphouse/run.py
@@ -11,7 +11,7 @@ def main():
             if not env_key.startswith("GH__"):
                 continue
             java_key = env_key.replace("GH", "graphouse", 1).lower().replace("__", ".").replace("_", "-")
-            print ("Property '%s' value: %s" % (java_key, value))
+            print("Property '%s' value: %s" % (java_key, value))
             graphouse_config.write("%s=%s\n" % (java_key, value))
 
     print("Generating graphouse vm options")
@@ -19,7 +19,7 @@ def main():
         vm_xmx = os.environ.get("GH_XMX", "4g")
         vm_xms = os.environ.get("GH_XMS", "256m")
         vm_xss = os.environ.get("GH_XSS", "2m")
-        print ("Xmx %s, xms %s, xss %s" % (vm_xmx, vm_xms, vm_xss))
+        print("Xmx %s, xms %s, xss %s" % (vm_xmx, vm_xms, vm_xss))
 
         graphouse_vm_config.write('-Xmx%s\n' % vm_xmx)
         graphouse_vm_config.write('-Xms%s\n' % vm_xms)
@@ -28,7 +28,7 @@ def main():
         graphouse_vm_config.write('-XX:+UseG1GC\n')
         graphouse_vm_config.write('-XX:MaxGCPauseMillis=1000\n')
 
-    print ("Starting graphouse")
+    print("Starting graphouse")
     subprocess.call("/opt/graphouse/bin/graphouse")
 
 
