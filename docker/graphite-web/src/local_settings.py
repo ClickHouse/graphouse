86edf024b1e24fe062ac15eab11bb5991ef22255
AndreevDm <AndreevDm@gmail.com>
2018-11-07 16:12:18 +0300

    MARKETINFRA-1878 graphite web docker (#83)

--- docker/graphite-web/src/local_settings.py
+++ docker/graphite-web/src/local_settings.py
@@ -23,3 +23,27 @@ if (os.getenv("TIME_ZONE") is not None):
 if (os.getenv("DATABASES") is not None):
     DATABASES = ast.literal_eval(os.getenv("DATABASES"))
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 1024 * 1024 * 1024,
+            'backupCount': 5,
+            'filename': '/var/log/graphite/django.log',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+if (os.path.exists("/opt/graphite/webapp/graphite/additional_local_settings.py")):
+    from graphite.additional_local_settings import *
+
---
+++ docker/graphite-web/src/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+service nginx start
+export PYTHONPATH=/opt/graphite/webapp
+exec /usr/local/bin/gunicorn wsgi --workers=4 --bind=127.0.0.1:8080 --log-file=/var/log/gunicorn.log --preload --pythonpath=/opt/graphite/webapp/graphite
