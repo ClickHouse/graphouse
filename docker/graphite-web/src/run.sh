dde8c119c5a72075120dc28465187313d625f264
AndreevDm <AndreevDm@gmail.com>
2019-01-16 15:01:44 +0300

    MARKETINFRA-4040 gunicorn limit-request-line (#105)
    * MARKETINFRA-4000 python-memcached removed
    
    * MARKETINFRA-4040 gunicorn limit-request-line

--- docker/graphite-web/src/run.sh
+++ docker/graphite-web/src/run.sh
@@ -9,4 +9,5 @@ exec /usr/local/bin/gunicorn wsgi --bind=127.0.0.1:8080 --log-file=/var/log/guni
     --worker-class=${GUNICORN_WORKER_CLASS:-'gthread'} \
     --workers=${GUNICORN_WORKERS:-8} --threads=${GUNICORN_THREADS:-4} \
     --max-requests=${GUNICORN_MAX_REQUSTS:-10000} --max-requests-jitter=${GUNICORN_MAX_REQUSTS_JITTER:-2000} \
-    --timeout=${GUNICORN_TIMEOUT:-60} --graceful-timeout=${GUNICORN_GRACEFULL_TIMEOUT:-60}
+    --timeout=${GUNICORN_TIMEOUT:-60} --graceful-timeout=${GUNICORN_GRACEFULL_TIMEOUT:-60} \
+    --limit-request-line=${GUNICORN_LIMIT_REQUEST_LINE:-8190}
