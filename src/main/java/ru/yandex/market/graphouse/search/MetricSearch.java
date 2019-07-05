f09199270f1924530ed3ad876220051019ec389a
Alexander Kedrik <alkedr@users.noreply.github.com>
2019-07-05 13:24:15 +0300

    MARKETINFRA-4669 Update MonitoringUnit and add setCriticalTimeout to MetricSearch monitoring (#124)

--- src/main/java/ru/yandex/market/graphouse/search/MetricSearch.java
+++ src/main/java/ru/yandex/market/graphouse/search/MetricSearch.java
@@ -125,6 +125,8 @@ public class MetricSearch implements InitializingBean, Runnable {
         this.metricValidator = metricValidator;
         this.retentionProvider = retentionProvider;
         monitoring.addUnit(metricSearchUnit);
+        metricSearchUnit.setWarningTimeout(3 * saveIntervalSeconds, TimeUnit.SECONDS);
+        metricSearchUnit.setCriticalTimeout(10 * saveIntervalSeconds, TimeUnit.SECONDS);
         ping.addUnit(metricTreeInitUnit);
         metricTreeInitUnit.critical("Initializing");
     }
