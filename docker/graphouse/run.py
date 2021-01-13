71cf3a1a69374ec9ab38448c731fc29931f537f2
Mr. Patrick <carbon2320@gmail.com>
2021-01-13 14:24:03 +0300

    MARKETINFRA-7684 Custom jvm options param has been added. Logs rotation fixed. (#159)
    Co-authored-by: Aleksei Malygin <malygin-me@yandex-team.ru>

--- docker/graphouse/run.py
+++ docker/graphouse/run.py
@@ -11,22 +11,32 @@ def main():
             if not env_key.startswith("GH__"):
                 continue
             java_key = env_key.replace("GH", "graphouse", 1).lower().replace("__", ".").replace("_", "-")
-            print("Property '%s' value: %s" % (java_key, value))
+            if 'password' not in java_key:
+                print("Property '%s' value: %s" % (java_key, value))
+
             graphouse_config.write("%s=%s\n" % (java_key, value))
 
     print("Generating graphouse vm options")
     with open('/etc/graphouse/graphouse.vmoptions', 'w') as graphouse_vm_config:
-        vm_xmx = os.environ.get("GH_XMX", "4g")
-        vm_xms = os.environ.get("GH_XMS", "256m")
-        vm_xss = os.environ.get("GH_XSS", "2m")
-        print("Xmx %s, xms %s, xss %s" % (vm_xmx, vm_xms, vm_xss))
 
-        graphouse_vm_config.write('-Xmx%s\n' % vm_xmx)
-        graphouse_vm_config.write('-Xms%s\n' % vm_xms)
-        graphouse_vm_config.write('-Xss%s\n' % vm_xss)
-        graphouse_vm_config.write('-XX:StringTableSize=10000000\n')
-        graphouse_vm_config.write('-XX:+UseG1GC\n')
-        graphouse_vm_config.write('-XX:MaxGCPauseMillis=1000\n')
+        # e.g. "GH_CUSTOM_JVM_OPTIONS=-XX:MaxRAM=4g\\n-XX:MaxRAMPercentage=80.0\\n-Xss2m\\n-XX:StringTableSize=10000000\\n-XX:+UseG1GC\\n-XX:MaxGCPauseMillis=1000\\n"
+        vm_options = os.environ.get("GH_CUSTOM_JVM_OPTIONS", None)
+        if vm_options is not None:
+            vm_options = vm_options.replace('\\n', '\n')
+            graphouse_vm_config.write(vm_options)
+            print("Custom options:\n%s" % (vm_options))
+        else:
+            vm_xmx = os.environ.get("GH_XMX", "4g")
+            vm_xms = os.environ.get("GH_XMS", "256m")
+            vm_xss = os.environ.get("GH_XSS", "2m")
+            print("Xmx %s, xms %s, xss %s" % (vm_xmx, vm_xms, vm_xss))
+
+            graphouse_vm_config.write('-Xmx%s\n' % vm_xmx)
+            graphouse_vm_config.write('-Xms%s\n' % vm_xms)
+            graphouse_vm_config.write('-Xss%s\n' % vm_xss)
+            graphouse_vm_config.write('-XX:StringTableSize=10000000\n')
+            graphouse_vm_config.write('-XX:+UseG1GC\n')
+            graphouse_vm_config.write('-XX:MaxGCPauseMillis=1000\n')
 
     print("Starting graphouse")
     subprocess.call("/opt/graphouse/bin/graphouse")
