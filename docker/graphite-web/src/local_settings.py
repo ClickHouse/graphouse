89d6a3f4e7f4f2c7c38614ca157a839f9fab6a0c
Mr. Patrick <carbon2320@gmail.com>
2021-02-16 13:29:22 +0300

    MARKETINFRA-8100 Formatters have been added. graphite-web version has… (#164)
    * MARKETINFRA-8100 Formatters have been added. graphite-web version has been increased.
    
    * MARKETINFRA-8100 oraclejdk9 removed
    
    Co-authored-by: Aleksei Malygin <malygin-me@yandex-team.ru>

--- docker/graphite-web/src/local_settings.py
+++ docker/graphite-web/src/local_settings.py
@@ -4,14 +4,15 @@ import os
 LOG_DIR = '/var/log/graphite'
 SECRET_KEY = '$(date +%s | sha256sum | base64 | head -c 64)'
 
-if (os.getenv("MEMCACHE_HOSTS") is not None):
+if os.getenv("MEMCACHE_HOSTS") is not None:
     MEMCACHE_HOSTS = [
         host.strip() for host in os.getenv("MEMCACHE_HOSTS").split(',')
     ]
 
-    #e.g. 'MEMCACHE_HOSTS' env var should be like this value =>   "hostname_1:port_1,...,hostname_n:port_n", n >= 1
-    if (os.getenv("RESOLVE_MEMCACHE_HOSTS_TO_IPV6") is not None):
+    # e.g. 'MEMCACHE_HOSTS' env var should be like this value =>   "hostname_1:port_1,...,hostname_n:port_n", n >= 1
+    if os.getenv("RESOLVE_MEMCACHE_HOSTS_TO_IPV6") is not None:
         import socket
+
         resolved_hosts = []
         for mh in MEMCACHE_HOSTS:
             (host, port) = mh.split(':')
@@ -20,25 +21,33 @@ if (os.getenv("MEMCACHE_HOSTS") is not None):
 
         MEMCACHE_HOSTS = resolved_hosts
 
-if (os.getenv("DEFAULT_CACHE_DURATION") is not None):
+if os.getenv("DEFAULT_CACHE_DURATION") is not None:
     DEFAULT_CACHE_DURATION = int(os.getenv("CACHE_DURATION"))
 
 STORAGE_FINDERS = (
     'graphite.graphouse.GraphouseFinder',
 )
 
-if (os.getenv("GRAPHOUSE_URL") is not None):
+if os.getenv("GRAPHOUSE_URL") is not None:
     GRAPHOUSE_URL = os.getenv("GRAPHOUSE_URL")
 
-if (os.getenv("TIME_ZONE") is not None):
+if os.getenv("TIME_ZONE") is not None:
     TIME_ZONE = os.getenv("TIME_ZONE")
 
-if (os.getenv("DATABASES") is not None):
+if os.getenv("DATABASES") is not None:
     DATABASES = ast.literal_eval(os.getenv("DATABASES"))
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        },
+    },
     'handlers': {
         'file': {
             'level': 'INFO',
@@ -46,6 +55,7 @@ LOGGING = {
             'maxBytes': 1024 * 1024 * 1024,
             'backupCount': 5,
             'filename': '/var/log/graphite/django.log',
+            'formatter': 'simple',
         },
     },
     'loggers': {
